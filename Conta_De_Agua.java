58e89d348b3845ab06060da26da5cbe61bc0059e
P1nhero <p1nhero2005@gmail.com>
2025-01-09 11:55:11 -0300

    Add files via upload

---
+++ Conta_De_Agua.java
@@ -0,0 +1,32 @@
+package conta_de_agua;
+import java.util.Scanner;
+public class Conta_De_Agua {
+
+	public static void main(String[] args) {
+		Scanner ler =  new Scanner(System.in);
+		
+		double gasto = ler.nextDouble();
+		double pagar = 0;
+		
+		if(gasto<30) {
+			pagar += 7;
+		}else
+			
+		if(gasto < 30) {
+			pagar += 7;
+			pagar += 20;
+			pagar+=()
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
